fadca831c29df1aaf3b2a0058d2d4beef0a6af49
zied-tech <66096316+zied-tech@users.noreply.github.com>
2022-06-03 14:01:41 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,43 +1,47 @@
 provider "azurerm" {
   features {}
 }
-
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistryadactimzied"
-  resource_group_name = var.name
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  sku                 = "Basic"
+  sku                 = "Standard"
   admin_enabled       = false
-  storage_account_id       = "/subscriptions/8dad328f-8daa-4be3-96fa-4713fd77f8fa/resourceGroups/cloud-shell-storage-westeurope/providers/Microsoft.Storage/storageAccounts/csb10032001f02bce76"
+  //storage_account_id       = "/subscriptions/8dad328f-8daa-4be3-96fa-4713fd77f8fa/resourceGroups/cloud-shell-storage-westeurope/providers/Microsoft.Storage/storageAccounts/csb10032001f02bce76"
 
 }
-resource "azurerm_kubernetes_cluster" "az-kc" {
-  name                = "k8s-cluster"
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = var.name
-  dns_prefix          = "aks-cluster-dns"
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_name
 
   default_node_pool {
-    name            = "default"
-    node_count      = 2
+    name            = "system"
+    node_count      = var.system_node_count
     vm_size         = "Standard_B2s"
+    type                = "VirtualMachineScaleSets"
+    availability_zones  = [1, 2, 3]
+    enable_auto_scaling = false
   }
 
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
-
-  role_based_access_control {
-    enabled = true
-  }
-  tags = {
-    environment = "k8s-env"
-  }
+  # service_principal {
+  #   client_id     = var.appId
+  #   client_secret = var.password
+  # }
+  identity{
+           type = "SystemAssigned"
+       }
 }
 
-resource "azurerm_role_assignment" "example" {
-  principal_id                     = azurerm_kubernetes_cluster.az-kc.kubelet_identity[0].object_id
+resource "azurerm_role_assignment" "role_acrpull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
